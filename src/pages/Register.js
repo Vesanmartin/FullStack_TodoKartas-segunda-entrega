400cdbfafb74f90f82d3147ea0cf3138d9c2c63d
spsp-sp <sa.parrac@duocuc.cl>
2025-11-07 14:58:26 -0300

    comentarios

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -4,6 +4,10 @@ import { useNavigate } from "react-router-dom";
 
 export default function Register(){
   const { register } = useAuth();
+
+  // Se declaran constantes de estado para los campos del formulario y errores
+  // Se usa useState porque los valores cambian conforme el usuario escribe
+  // Así, se maneja en tiempo real las validaciones.
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
@@ -13,9 +17,12 @@ export default function Register(){
 
   const submit=(e)=>{
     e.preventDefault(); setErr("");
+    // Validacion de contraseñas coincidentes
     if(pw!==pw2){ setErr("Las contraseñas no coinciden."); return; }
     try{
+      //Se llama a la función register del contexto de autenticación
       register({name, email, password:pw});
+      // Redirigir al usuario a la página principal tras el registro exitoso
       nav("/");
     }catch(ex){ setErr(ex.message); }
   };
@@ -23,18 +30,22 @@ export default function Register(){
   return (
     <div className="container mt-4" style={{maxWidth:520}}>
       <h2 className="mb-3">Crear cuenta</h2>
+      // Mostrar mensaje de error si existe 
       {err && <div className="alert alert-danger py-2">{err}</div>}
       <form onSubmit={submit}>
         <div className="mb-3">
           <label className="form-label">Nombre</label>
+          // Incluye validación de campo requerido (no vacío)
           <input className="form-control" value={name} onChange={e=>setName(e.target.value)} required/>
         </div>
         <div className="mb-3">
           <label className="form-label">Correo</label>
+          // Incluye validación de campo requerido y formato de email
           <input type="email" className="form-control" value={email} onChange={e=>setEmail(e.target.value)} required/>
         </div>
         <div className="mb-3">
           <label className="form-label">Contraseña</label>
+          // Incluye validación de campo requerido y formato password - no visible - 
           <input type="password" className="form-control" value={pw} onChange={e=>setPw(e.target.value)} required/>
         </div>
         <div className="mb-3">
