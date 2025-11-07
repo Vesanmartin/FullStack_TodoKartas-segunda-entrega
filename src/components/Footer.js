f582d1d6ba27726bbd2e127ecf4ea14024616493
Vesanmartin <ve.sanmartin@duocuc.cl>
2025-11-07 00:11:18 -0300

    footer mas bonito

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import '../styles.css'; 
 
 export default function Footer(){
-    return(
-        <footer className="py-4 text-center text-muted border-top">
-        <small>© 2025 TodOKartas</small>
-        </footer>
-)
+    return (
+    <footer className="footer py-4 text-center text-muted border-top">
+      
+      <div className="footer__image-container">
+        <img 
+          src="/assets/images/Fondobanner_magic2.jpg" 
+          alt="Fondo del Footer"
+          className="footer__image"
+        />
+      </div>
+      <div className="footer__content">
+        <small>© 2025 TodoKartas</small>
+      </div>
+    </footer>
+  );
 }
